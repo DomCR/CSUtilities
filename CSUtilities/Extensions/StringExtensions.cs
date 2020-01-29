4716b40402f939b7aef85ef15c2192a5eefd930a
Dom <DomCR@users.noreply.github.com>
2020-01-29 16:03:48 +0100

    Initial commit

---
+++ CSUtilities/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSUtilities.Extensions
+{
+    static class StringExtensions
+    {
+        /// <summary>
+        /// Gets a string and returns an array of bytes.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] ToByteArray(this string value)
+        {
+            return Enumerable.Range(0, value.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(value.Substring(x, 2), 16))
+                .ToArray();
+        }
+    }
+}
