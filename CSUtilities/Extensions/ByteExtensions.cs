5755cb8d327e4ec561b5d7071fa63eb59ec7b265
Dom <DomCR@users.noreply.github.com>
2020-01-30 17:17:11 +0100

    minor update

---
+++ CSUtilities/Extensions/ByteExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSUtilities.Extensions
+{
+    internal static class ByteExtensions
+    {
+        /// <summary>
+        /// Convert a byte array into a hex string array.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string ToStringHex(this byte[] array)
+        {
+            StringBuilder hex = new StringBuilder(array.Length * 2);
+            foreach (byte b in array)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
--- CSUtilities/Extensions/StringExtensions.cs
+++ CSUtilities/Extensions/StringExtensions.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSUtilities.Extensions
 {
-    static class StringExtensions
+    internal static class StringExtensions
     {
+        /// <summary>
+        /// Return an array with all the lines.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string[] GetLines(this string text)
+        {
+            //Guard
+            if (text == null)
+                return null;
+
+            text = text.Replace("\r\n", "\n");
+
+            return text.Split('\n');
+        }
         /// <summary>
         /// Gets a string and returns an array of bytes.
         /// </summary>
@@ -17,6 +33,105 @@ namespace CSUtilities.Extensions
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(value.Substring(x, 2), 16))
                 .ToArray();
+        }
+        /// <summary>
+        /// Returns the first string between 2 characters.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static string ReadBetween(this string line, char start, char end)
+        {
+            var stack = new Stack<int>();
+            bool isFirst = true;
+            string group = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == start)
+                {
+                    //Save the index of the open character
+                    stack.Push(i);
+
+                    //Save the index of the first open char
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        continue;
+                    }
+                }
+                else if (line[i] == end)
+                {
+                    //Check if the sequence contains an open
+                    if (!isFirst)
+                    {
+                        stack.Pop();
+
+                        //Closing character found
+                        if (!stack.Any())
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //If the first open character have been found, start reading the string
+                if (!isFirst)
+                {
+                    group += line[i];
+                }
+            }
+
+            return group;
+        }
+        /// <summary>
+        /// Reads a string until it finds a character.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="c">Character to find.</param>
+        /// <returns></returns>
+        public static string ReadUntil(this string line, char c)
+        {
+            string value = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == c)
+                    break;
+                else
+                    value += line[i];
+            }
+
+            return value;
+        }
+        public static string ReadUntil(this string line, char c, out string rest)
+        {
+            string value = "";
+            rest = "";
+
+            bool reading = true;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == c)
+                    break;
+                else if (reading)
+                    value += line[i];
+                else
+                    rest += line[i];
+            }
+
+            return value;
+
+        }
+        public static string RemoveStartWhitespaces(this string line)
+        {
+            while (line.StartsWith(" "))
+            {
+                line = line.Remove(0, 1);
+            }
+
+            return line;
         }
     }
 }
