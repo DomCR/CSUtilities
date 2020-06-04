f7e00a03ca800cabb1212459b8520ef54f6c15eb
Dom93 <domenech93@gmail.com>
2020-06-04 17:22:18 +0200

    dll library added, first wpf utility classes

---
+++ CSUtilities/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CSUtilities.ViewModels
+{
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void onPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
