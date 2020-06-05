d770607398ce679f32c05eff6a201e216493ce04
Dom93 <domenech93@gmail.com>
2020-06-05 10:35:13 +0200

    wpf progressbar initial

--- CSUtilities/Extensions/IEnumerableExtensions.cs
+++ CSUtilities/Extensions/IEnumerableExtensions.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace CSUtilities.Extensions
 {
+    /// <summary>
+    /// Estensions for <see cref="IEnumerable{T}"/>.
+    /// </summary>
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// Return true if the collection is empty.
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <returns></returns>
         public static bool IsEmpty(this IEnumerable<object> enumerable)
         {
             return enumerable.GetEnumerator() == null;
