fa9cc7c4ac8b042f333039f2c3f97d969034dc4c
Dom93 <domenech93@gmail.com>
2020-07-13 11:32:53 +0200

    Enum utils updated

--- CSUtilities/EnumUtils.cs
+++ CSUtilities/EnumUtils.cs
@@ -5,17 +5,21 @@ using System.Text;
 
 namespace CSUtilities
 {
-   public static class EnumUtils
-    {
-        [Obsolete("Use Type.GetValues()")]
-        public static IEnumerable<T> GetValues<T>()
-        {
-            return Enum.GetValues(typeof(T)).Cast<T>();
-        }
-        [Obsolete("Use Type.GetNames()")]
-        public static IEnumerable<string> GetValuesNames<T>()
-        {
-            return Enum.GetValues(typeof(T)).Cast<T>().Select(o => o.ToString());
-        }
-    }
+	public static class EnumUtils
+	{
+		[Obsolete("Use Type.GetValues()")]
+		public static IEnumerable<T> GetValues<T>()
+		{
+			return Enum.GetValues(typeof(T)).Cast<T>();
+		}
+		[Obsolete("Use Type.GetNames()")]
+		public static IEnumerable<string> GetValuesNames<T>()
+		{
+			return Enum.GetValues(typeof(T)).Cast<T>().Select(o => o.ToString());
+		}
+		public static T GetValueByName<T>(string name)
+		{
+			return Enum.GetValues(typeof(T)).Cast<T>().FirstOrDefault(o => o.ToString() == name);
+		}
+	}
 }
