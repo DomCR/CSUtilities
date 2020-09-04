8d62fc0970b2c99dd5eb910c02fd87d83fc5ef94
Dom93 <domenech93@gmail.com>
2020-09-04 12:48:19 +0200

    text encodings

--- CSUtilities/Extensions/ByteExtensions.cs
+++ CSUtilities/Extensions/ByteExtensions.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSUtilities.Extensions
 {
-    internal static class ByteExtensions
-    {
-        /// <summary>
-        /// Convert a byte array into a hex string array.
-        /// </summary>
-        /// <param name="array"></param>
-        /// <returns></returns>
-        public static string ToStringHex(this byte[] array)
-        {
-            StringBuilder hex = new StringBuilder(array.Length * 2);
-            foreach (byte b in array)
-                hex.AppendFormat("{0:x2}", b);
-            return hex.ToString();
-        }
-    }
+	public static class ByteExtensions
+	{
+		/// <summary>
+		/// Convert a byte array into a hex string array.
+		/// </summary>
+		/// <param name="array"></param>
+		/// <returns></returns>
+		public static string ToHexString(this IEnumerable<byte> array)
+		{
+			StringBuilder hex = new StringBuilder(array.Count() * 2);
+			foreach (byte b in array)
+				hex.AppendFormat("{0:x2}", b);
+			return hex.ToString();
+		}
+	}
 }
