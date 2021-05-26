bf5f964cfa03411e28a3a6af75c22a01e2c7c93f
Albert Domenech <46492487+DomCR@users.noreply.github.com>
2021-05-26 11:17:48 +0200

    ienumerable implementation

--- CSUtilities/AppDomainUtils.cs
+++ CSUtilities/AppDomainUtils.cs
@@ -8,18 +8,17 @@ namespace CSUtilities
 {
 	internal static class AppDomainUtils
 	{
-		public static Type[] GetTypesOfInterface<T>()
+		public static IEnumerable<Type> GetTypesOfInterface<T>()
 		{
 			return AppDomain.CurrentDomain.GetAssemblies()
 				.SelectMany(s => s.GetTypes())
-				.Where(p => p.GetInterface(typeof(T).FullName) != null)
-				.ToArray();
+				.Where(p => p.GetInterface(typeof(T).FullName) != null);
 		}
 
-		public static Type[] GetTypesWithAttribute<T>() where T : Attribute
+		public static IEnumerable<Type> GetTypesWithAttribute<T>() where T : Attribute
 		{
 			return AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes())
-			.Where(p => p.GetCustomAttribute<T>() != null).ToArray();
+			.Where(p => p.GetCustomAttribute<T>() != null);
 		}
 	}
 }
