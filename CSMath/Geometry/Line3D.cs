4bc3bc39de358986e2f13cd2f8c9a27d2f1940ea
DomCR <DomCR@users.noreply.github.com>
2023-04-28 14:06:23 +0200

    files

---
+++ CSMath/Geometry/Line3D.cs
@@ -0,0 +1,11 @@
+﻿namespace CSMath.Geometry
+{
+	public struct Line3D : ILine<XYZ>
+	{
+		/// <inheritdoc/>
+		public XYZ Origin { get; set; }
+
+		/// <inheritdoc/>
+		public XYZ Direction { get; set; }
+	}
+}
