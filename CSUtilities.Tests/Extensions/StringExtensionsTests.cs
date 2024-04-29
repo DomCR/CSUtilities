f82c482b20316471879debba78da301379bbb270
DomCR <DomCR@users.noreply.github.com>
2024-04-29 10:46:45 +0200

    test

---
+++ CSUtilities.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSUtilities.Tests.Extensions
+{
+	public class StringExtensionsTests
+	{
+		[Fact]
+		public void GetStringValueTest()
+		{
+
+		}
+	}
+}
