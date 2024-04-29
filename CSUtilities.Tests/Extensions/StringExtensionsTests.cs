4d29042204910d0ebf4d87df4cd515ed7fa46840
DomCR <DomCR@users.noreply.github.com>
2024-04-29 10:47:26 +0200

    TrowIfNullOrEmpty
    TrowIfNullOrEmpty

---
+++ CSUtilities.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using CSUtilities.Extensions;
+using Xunit;
+
+namespace CSUtilities.Tests.Extensions
+{
+	public class StringExtensionsTests
+	{
+		[Fact]
+		public void IsNullOrEmptyTest()
+		{
+			string n = null;
+			Assert.True(n.IsNullOrEmpty());
+		}
+	}
+}
