95e916a62f0ddf59cd165e0606a74ba044919056
DomCR <DomCR@users.noreply.github.com>
2024-05-27 10:46:45 +0200

    BoundingBox Merge

--- CSMath/BoundingBox.cs
+++ CSMath/BoundingBox.cs
@@ -1,22 +1,22 @@
 ﻿namespace CSMath
 {
 	/// <summary>
-	/// Bounding box representation aligned to XYZ axis
+	/// Bounding box representation aligned to XYZ axis.
 	/// </summary>
 	public struct BoundingBox
 	{
 		/// <summary>
-		/// Get the min corner of the bounding box
+		/// Get the min corner of the bounding box.
 		/// </summary>
 		public XYZ Min { get; set; }
 
 		/// <summary>
-		/// Get the max corner of the bounding box
+		/// Get the max corner of the bounding box.
 		/// </summary>
 		public XYZ Max { get; set; }
 
 		/// <summary>
-		/// Center of the box
+		/// Center of the box.
 		/// </summary>
 		public XYZ Center
 		{
@@ -27,7 +27,7 @@
 		}
 
 		/// <summary>
-		/// Bounding box constructor with 2 points
+		/// Bounding box constructor with 2 points.
 		/// </summary>
 		/// <param name="min"></param>
 		/// <param name="max"></param>
@@ -38,7 +38,7 @@
 		}
 
 		/// <summary>
-		/// Bounding box contructor
+		/// Bounding box contructor.
 		/// </summary>
 		/// <param name="minX"></param>
 		/// <param name="minY"></param>
@@ -49,6 +49,25 @@
 		public BoundingBox(double minX, double minY, double minZ, double maxX, double maxY, double maxZ)
 		{
 			this = new BoundingBox(new XYZ(minX, minY, minZ), new XYZ(maxX, maxY, maxZ));
+		}
+
+		/// <summary>
+		/// Merge 2 boxes into the common one.
+		/// </summary>
+		/// <param name="box"></param>
+		/// <returns>The merged box.</returns>
+		public BoundingBox Merge(BoundingBox box)
+		{
+			var min = new XYZ(
+				System.Math.Min(this.Min.X, box.Min.X),
+				System.Math.Min(this.Min.Y, box.Min.Y),
+				System.Math.Min(this.Min.Z, box.Min.Z));
+			var max = new XYZ(
+				System.Math.Max(this.Max.X, box.Max.X),
+				System.Math.Max(this.Max.Y, box.Max.Y),
+				System.Math.Max(this.Max.Z, box.Max.Z));
+
+			return new BoundingBox(min, max);
 		}
 	}
 }
