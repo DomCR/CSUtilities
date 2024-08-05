eca6f7ebe91058bcc89fb818fb14aae050a91152
DomCR <DomCR@users.noreply.github.com>
2024-08-05 18:36:41 +0200

    BoundingBox

--- CSMath/BoundingBox.cs
+++ CSMath/BoundingBox.cs
@@ -1,10 +1,24 @@
-﻿namespace CSMath
+﻿using System.Collections.Generic;
+
+namespace CSMath
 {
 	/// <summary>
 	/// Bounding box representation aligned to XYZ axis.
 	/// </summary>
 	public struct BoundingBox
 	{
+		/// <summary>
+		/// Instance of a null bounding box.
+		/// </summary>
+		public static readonly BoundingBox Null = new BoundingBox(BoundingBoxExtent.Null);
+
+		/// <summary>
+		/// Instance of an infinite bounding box.
+		/// </summary>
+		public static readonly BoundingBox Infinite = new BoundingBox(BoundingBoxExtent.Infinite);
+
+		public BoundingBoxExtent Extent { get; }
+
 		/// <summary>
 		/// Get the min corner of the bounding box.
 		/// </summary>
@@ -26,6 +40,18 @@
 			}
 		}
 
+		private BoundingBox(BoundingBoxExtent extent)
+		{
+			this.Extent = extent;
+		}
+
+		public BoundingBox(XYZ point)
+		{
+			this.Extent = BoundingBoxExtent.Point;
+			this.Min = point;
+			this.Max = point;
+		}
+
 		/// <summary>
 		/// Bounding box constructor with 2 points.
 		/// </summary>
@@ -33,6 +59,15 @@
 		/// <param name="max"></param>
 		public BoundingBox(XYZ min, XYZ max)
 		{
+			if (min == max)
+			{
+				this.Extent = BoundingBoxExtent.Point;
+			}
+			else
+			{
+				this.Extent = BoundingBoxExtent.Finite;
+			}
+
 			this.Min = new XYZ(System.Math.Min(min.X, max.X), System.Math.Min(min.Y, max.Y), System.Math.Min(min.Z, max.Z));
 			this.Max = new XYZ(System.Math.Max(min.X, max.X), System.Math.Max(min.Y, max.Y), System.Math.Max(min.Z, max.Z));
 		}
@@ -68,6 +103,23 @@
 				System.Math.Max(this.Max.Z, box.Max.Z));
 
 			return new BoundingBox(min, max);
+		}
+
+		/// <summary>
+		/// Create a bounding box from a collection of points.
+		/// </summary>
+		/// <param name="points"></param>
+		/// <returns></returns>
+		public static BoundingBox FromPoints(IEnumerable<XYZ> points)
+		{
+			BoundingBox boundingBox = Null;
+
+			foreach (var point in points)
+			{
+				boundingBox = boundingBox.Merge(new BoundingBox(point));
+			}
+
+			return boundingBox;
 		}
 	}
 }
