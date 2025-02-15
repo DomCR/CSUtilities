0d4b57f3977f151d2a5b78c53a31050acab48210
DomCR <DomCR@users.noreply.github.com>
2025-02-15 18:52:16 +0100

    quaternion

--- CSMath/Matrix4.cs
+++ CSMath/Matrix4.cs
@@ -468,5 +468,230 @@ namespace CSMath
 				m20 == other.m20 && m21 == other.m21 && m23 == other.m23 &&
 				m30 == other.m30 && m31 == other.m31 && m32 == other.m32;
 		}
+
+		/// <summary>
+		/// Creates a translation matrix.
+		/// </summary>
+		/// <param name="position">The amount to translate in each axis.</param>
+		/// <returns>The translation matrix.</returns>
+		public static Matrix4 CreateTranslation(XYZ position)
+		{
+			return Matrix4.CreateTranslation(position.X, position.Y, position.Z);
+		}
+
+		/// <summary>
+		/// Creates a translation matrix.
+		/// </summary>
+		/// <param name="xPosition">The amount to translate on the X-axis.</param>
+		/// <param name="yPosition">The amount to translate on the Y-axis.</param>
+		/// <param name="zPosition">The amount to translate on the Z-axis.</param>
+		/// <returns>The translation matrix.</returns>
+		public static Matrix4 CreateTranslation(double xPosition, double yPosition, double zPosition)
+		{
+			Matrix4 result;
+
+			result.m00 = 1.0f;
+			result.m01 = 0.0f;
+			result.m02 = 0.0f;
+			result.m03 = 0.0f;
+			result.m10 = 0.0f;
+			result.m11 = 1.0f;
+			result.m12 = 0.0f;
+			result.m13 = 0.0f;
+			result.m20 = 0.0f;
+			result.m21 = 0.0f;
+			result.m22 = 1.0f;
+			result.m23 = 0.0f;
+
+			result.m30 = xPosition;
+			result.m31 = yPosition;
+			result.m32 = zPosition;
+			result.m33 = 1.0f;
+
+			return result;
+		}
+
+		/// <summary>
+		/// Creates a scaling matrix.
+		/// </summary>
+		/// <param name="scale">The vector containing the amount to scale by on each axis.</param>
+		/// <returns>The scaling matrix.</returns>
+		public static Matrix4 CreateScale(XYZ scale)
+		{
+			return CreateScale(scale, XYZ.Zero);
+		}
+
+		/// <summary>
+		/// Creates a scaling matrix with a center point.
+		/// </summary>
+		/// <param name="scale">The vector containing the amount to scale by on each axis.</param>
+		/// <param name="centerPoint">The center point.</param>
+		/// <returns>The scaling matrix.</returns>
+		public static Matrix4 CreateScale(XYZ scale, XYZ centerPoint)
+		{
+			Matrix4 result;
+
+			double tx = centerPoint.X * (1 - scale.X);
+			double ty = centerPoint.Y * (1 - scale.Y);
+			double tz = centerPoint.Z * (1 - scale.Z);
+
+			result.m00 = scale.X;
+			result.m01 = 0.0f;
+			result.m02 = 0.0f;
+			result.m03 = 0.0f;
+			result.m10 = 0.0f;
+			result.m11 = scale.Y;
+			result.m12 = 0.0f;
+			result.m13 = 0.0f;
+			result.m20 = 0.0f;
+			result.m21 = 0.0f;
+			result.m22 = scale.Z;
+			result.m23 = 0.0f;
+			result.m30 = tx;
+			result.m31 = ty;
+			result.m32 = tz;
+			result.m33 = 1.0f;
+
+			return result;
+		}
+
+		/// <summary>
+		/// Creates a uniform scaling matrix that scales equally on each axis.
+		/// </summary>
+		/// <param name="scale">The uniform scaling factor.</param>
+		/// <returns>The scaling matrix.</returns>
+		public static Matrix4 CreateScale(double scale)
+		{
+			return CreateScale(scale, XYZ.Zero);
+		}
+
+		/// <summary>
+		/// Creates a uniform scaling matrix that scales equally on each axis with a center point.
+		/// </summary>
+		/// <param name="scale">The uniform scaling factor.</param>
+		/// <param name="centerPoint">The center point.</param>
+		/// <returns>The scaling matrix.</returns>
+		public static Matrix4 CreateScale(double scale, XYZ centerPoint)
+		{
+			return CreateScale(new XYZ(scale), centerPoint);
+		}
+
+		public static Matrix4 CreateRotationMatrix(XYZ angles)
+		{
+			return CreateRotationMatrix(angles.X, angles.Y, angles.Z);
+		}
+
+		/// <summary>
+		/// Creates a rotation matrix.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		/// <returns></returns>
+		public static Matrix4 CreateRotationMatrix(double x, double y, double z)
+		{
+			double cosx = Math.Cos(x);
+			double cosy = Math.Cos(y);
+			double cosz = Math.Cos(z);
+
+			double sinx = Math.Sin(x);
+			double siny = Math.Sin(y);
+			double sinz = Math.Sin(z);
+
+			//X rotation 
+			Matrix4 rx = new Matrix4(
+				1, 0, 0, 0,
+				0, cosx, sinx, 0,
+				0, -sinx, cosx, 0,
+				0, 0, 0, 1);
+
+			//Y rotation 
+			Matrix4 ry = new Matrix4(
+				cosy, 0, -siny, 0,
+				0, 1, 0, 0,
+				siny, 0, cosy, 0,
+				0, 0, 0, 1);
+
+			//Z rotation 
+			Matrix4 rz = new Matrix4(
+				cosz, -sinz, 0, 0,
+				sinz, cosz, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1);
+
+			return rx * ry * rz;
+		}
+
+		/// <summary>
+		/// Creates a matrix that rotates around an arbitrary vector.
+		/// </summary>
+		/// <param name="axis">The axis to rotate around.</param>
+		/// <param name="angle">The angle to rotate around the given axis, in radians.</param>
+		/// <returns>The rotation matrix.</returns>
+		public static Matrix4 CreateFromAxisAngle(XYZ axis, double angle)
+		{
+			// a: angle
+			// x, y, z: unit vector for axis.
+			//
+			// Rotation matrix M can compute by using below equation.
+			//
+			//        T               T
+			//  M = uu + (cos a)( I-uu ) + (sin a)S
+			//
+			// Where:
+			//
+			//  u = ( x, y, z )
+			//
+			//      [  0 -z  y ]
+			//  S = [  z  0 -x ]
+			//      [ -y  x  0 ]
+			//
+			//      [ 1 0 0 ]
+			//  I = [ 0 1 0 ]
+			//      [ 0 0 1 ]
+			//
+			//
+			//     [  xx+cosa*(1-xx)   yx-cosa*yx-sina*z zx-cosa*xz+sina*y ]
+			// M = [ xy-cosa*yx+sina*z    yy+cosa(1-yy)  yz-cosa*yz-sina*x ]
+			//     [ zx-cosa*zx-sina*y zy-cosa*zy+sina*x   zz+cosa*(1-zz)  ]
+			//
+			double x = axis.X, y = axis.Y, z = axis.Z;
+			double sa = (double)Math.Sin(angle), ca = (double)Math.Cos(angle);
+			double xx = x * x, yy = y * y, zz = z * z;
+			double xy = x * y, xz = x * z, yz = y * z;
+
+			Matrix4 result = new Matrix4();
+
+			result.m00 = xx + ca * (1.0f - xx);
+			result.m01 = xy - ca * xy + sa * z;
+			result.m02 = xz - ca * xz - sa * y;
+			result.m03 = 0.0f;
+			result.m10 = xy - ca * xy - sa * z;
+			result.m11 = yy + ca * (1.0f - yy);
+			result.m12 = yz - ca * yz + sa * x;
+			result.m13 = 0.0f;
+			result.m20 = xz - ca * xz + sa * y;
+			result.m21 = yz - ca * yz - sa * x;
+			result.m22 = zz + ca * (1.0f - zz);
+			result.m23 = 0.0f;
+			result.m30 = 0.0f;
+			result.m31 = 0.0f;
+			result.m32 = 0.0f;
+			result.m33 = 1.0f;
+
+			return result;
+		}
+
+		/// <summary>
+		/// Builds a matrix that scales along the x-axis, y-axis, and z-axis.
+		/// </summary>
+		public static Matrix4 CreateScalingMatrix(double x, double y, double z)
+		{
+			return new Matrix4(
+				x, 0.0, 0.0, 0.0,
+				0.0, y, 0.0, 0.0,
+				0.0, 0.0, z, 0.0,
+				0.0, 0.0, 0.0, 1.0);
+		}
 	}
 }
