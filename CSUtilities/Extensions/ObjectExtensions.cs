2ef2a715dabd7218f29bf10bd7c86f7e37e425e8
DomCR <DomCR@users.noreply.github.com>
2025-03-19 18:29:46 +0100

    inrage

--- CSUtilities/Extensions/ObjectExtensions.cs
+++ CSUtilities/Extensions/ObjectExtensions.cs
@@ -54,6 +54,15 @@ namespace CSUtilities.Extensions
 			}
 		}
 
+		public static void InRange<T>(this T value, T min, T max, bool inclusive = true, [CallerMemberName] string name = null)
+		where T : struct, IComparable<T>
+		{
+			if (value.CompareTo(max) >= 1 && value.CompareTo(min) <= -1)
+			{
+				throw new ArgumentOutOfRangeException(name, value, $"{name} valid values are from {min} to {max}");
+			}
+		}
+
 		public static void InRange<T>(this T value, T min, T max, string message, bool inclusive = true, [CallerMemberName] string name = null)
 			where T : struct, IComparable<T>
 		{
