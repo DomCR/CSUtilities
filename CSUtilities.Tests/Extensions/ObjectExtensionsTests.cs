b7f58a3b8dfb5d86843b6cb44bdd9e469601f309
DomCR <DomCR@users.noreply.github.com>
2025-03-26 15:56:48 +0100

    test

---
+++ CSUtilities.Tests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using CSUtilities.Extensions;
+using System;
+using Xunit;
+
+namespace CSUtilities.Tests.Extensions
+{
+	public class ObjectExtensionsTests
+	{
+		[Fact]
+		public void ThrowIfTest()
+		{
+			int zero = 0;
+
+			Assert.Throws<ArgumentOutOfRangeException>(() => zero.ThrowIf<int, ArgumentOutOfRangeException>((value) =>
+				{
+					return value == 0;
+				}));
+		}
+	}
+}
