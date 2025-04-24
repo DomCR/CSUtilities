622d846783540a497d4a30a43da2aa7337e031d2
DomCR <DomCR@users.noreply.github.com>
2025-04-24 19:22:41 +0200

    Curves

---
+++ CSMath/Geometry/CurveExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSMath.Geometry
+{
+	public static class CurveExtensions
+	{
+		public static XYZ PolarCoordinateRelativeToCenter(double angle, XYZ center,  XYZ normal, XYZ startPoint, double ratio = 1)
+		{
+			XYZ prep = XYZ.Cross(normal, startPoint);
+			double radius = startPoint.Normalize().GetLength();
+			return radius * (Math.Cos(angle) * startPoint + (double)ratio * Math.Sin(angle) * prep) + center;
+		}
+
+		public static List<XYZ> PolygonalVertexes(int precision, XYZ center, double startAngle, double endAngle, XYZ normal, XYZ startPoint, double ratio = 1)
+		{
+			if (precision < 2)
+			{
+				throw new ArgumentOutOfRangeException(nameof(precision), precision, "The precision must be equal or greater than two.");
+			}
+
+			List<XYZ> points = new();
+
+			double start = startAngle;
+			double end = endAngle;
+			if (end <= start)
+			{
+				end += MathHelper.TwoPI;
+			}
+
+			XYZ prep = XYZ.Cross(normal, startPoint);
+
+			double portion = startPoint.Normalize().GetLength();
+			double radius = ratio * portion;
+
+			double delta = (end - start) / (precision - 1);
+
+			for (int i = 0; i < precision; i++, start += delta)
+			{
+				points.Add(portion * Math.Cos(start) * startPoint + radius * Math.Sin(start) * prep + center);
+			}
+
+			return points;
+		}
+	}
+}
---
+++ CSMath/Geometry/ICurve.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSMath.Geometry
+{
+	public interface ICurve
+	{
+		List<XYZ> PolygonalVertexes(int precision);
+
+		XYZ PolarCoordinateRelativeToCenter(double angle);
+	}
+}
