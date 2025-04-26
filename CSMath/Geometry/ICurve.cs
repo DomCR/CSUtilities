d0925279e5505c8e028b78c36f0b99de55841341
DomCR <DomCR@users.noreply.github.com>
2025-04-26 11:31:15 +0200

    docs

--- CSMath/Geometry/ICurve.cs
+++ CSMath/Geometry/ICurve.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace CSMath.Geometry
 {
 	public interface ICurve
 	{
+		/// <summary>
+		///Specifies the center of the curve.
+		/// </summary>
+		XYZ Center { get; }
+
+		/// <summary>
+		/// Ratio of minor axis to major axis.
+		/// </summary>
+		/// <remarks>
+		/// Value is 1 for a circle.
+		/// </remarks>
+		double RadiusRatio { get; }
+
+		/// <summary>
+		/// Converts the curve in a list of vertexes.
+		/// </summary>
+		/// <param name="precision">Number of vertexes generated.</param>
+		/// <returns>A list vertexes that represents the curve expressed in object coordinate system.</returns>
 		List<XYZ> PolygonalVertexes(int precision);
 
+		/// <summary>
+		/// Calculate the local point on the curve for a given angle relative to the center.
+		/// </summary>
+		/// <param name="angle">Angle in radians.</param>
+		/// <returns>A local point on the curve for the given angle relative to the center.</returns>
 		XYZ PolarCoordinateRelativeToCenter(double angle);
 	}
 }
