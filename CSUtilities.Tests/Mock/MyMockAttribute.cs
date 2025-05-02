288b91f83150c52a9366a4f4298703b717697dd5
DomCR <DomCR@users.noreply.github.com>
2025-05-02 11:03:11 +0200

    AppDomainUtilsTests

---
+++ CSUtilities.Tests/Mock/MyMockAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSUtilities.Tests.Mock
+{
+	[System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+	internal sealed class MyMockAttribute : Attribute
+	{
+		// This is a named argument
+		public int NamedInt { get; set; }
+
+		public string PositionalString
+		{
+			get { return positionalString; }
+		}
+
+		// See the attribute guidelines at
+		//  http://go.microsoft.com/fwlink/?LinkId=85236
+		private readonly string positionalString;
+
+		// This is a positional argument
+		public MyMockAttribute(string positionalString)
+		{
+			this.positionalString = positionalString;
+		}
+	}
+}
