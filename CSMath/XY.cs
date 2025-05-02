1316eb6a7e3c70628ff2c824d3a2eb17af74da4e
DomCR <DomCR@users.noreply.github.com>
2025-05-02 19:35:58 +0200

    Polar

--- CSMath/XY.cs
+++ CSMath/XY.cs
@@ -4,10 +4,8 @@ namespace CSMath
 {
 	public partial struct XY : IVector, IEquatable<XY>
 	{
-		public readonly static XY NaN = new XY(double.NaN);
-		public readonly static XY Zero = new XY(0, 0);
-		public readonly static XY AxisX = new XY(1, 0);
-		public readonly static XY AxisY = new XY(0, 1);
+		/// <inheritdoc/>
+		public uint Dimension { get { return 2; } }
 
 		/// <summary>
 		/// Specifies the X-value of the vector component
@@ -19,39 +17,10 @@ namespace CSMath
 		/// </summary>
 		public double Y { get; set; }
 
-		/// <inheritdoc/>
-		public uint Dimension { get { return 2; } }
-
-		/// <inheritdoc/>
-		public double this[int index]
-		{
-			get
-			{
-				switch (index)
-				{
-					case 0:
-						return X;
-					case 1:
-						return Y;
-					default:
-						throw new IndexOutOfRangeException($"The index must be between 0 and {this.Dimension}.");
-				}
-			}
-			set
-			{
-				switch (index)
-				{
-					case 0:
-						X = value;
-						break;
-					case 1:
-						Y = value;
-						break;
-					default:
-						throw new IndexOutOfRangeException($"The index must be between 0 and {this.Dimension}.");
-				}
-			}
-		}
+		public static readonly XY AxisX = new XY(1, 0);
+		public static readonly XY AxisY = new XY(0, 1);
+		public static readonly XY NaN = new XY(double.NaN);
+		public static readonly XY Zero = new XY(0, 0);
 
 		/// <summary>
 		/// Constructor with the coordinate components
@@ -71,15 +40,6 @@ namespace CSMath
 		public XY(double value) : this(value, value) { }
 
 		/// <summary>
-		/// Get the angle
-		/// </summary>
-		/// <returns>Angle in radians</returns>
-		public double GetAngle()
-		{
-			return Math.Atan2(Y, X);
-		}
-
-		/// <summary>
 		/// Computes the cross product of two coordinates.
 		/// </summary>
 		/// <param name="xy1">The first coordinate.</param>
@@ -88,6 +48,19 @@ namespace CSMath
 		public static double Cross(XY xy1, XY xy2)
 		{
 			return xy1.X * xy2.Y - xy1.Y * xy2.X;
+		}
+
+		/// <summary>
+		/// Obtains the polar point of another point.
+		/// </summary>
+		/// <param name="u">Reference point.</param>
+		/// <param name="distance">Distance from point u.</param>
+		/// <param name="angle">Angle in radians.</param>
+		/// <returns>The polar point of the specified point.</returns>
+		public static XY Polar(XY u, double distance, double angle)
+		{
+			XY dir = new XY(Math.Cos(angle), Math.Sin(angle));
+			return u + dir * distance;
 		}
 
 		/// <summary>
@@ -101,27 +74,6 @@ namespace CSMath
 			double sin = Math.Sin(angle);
 			double cos = Math.Cos(angle);
 			return new XY(value.X * cos - value.Y * sin, value.X * sin + value.Y * cos);
-		}
-
-		/// <summary>
-		/// Get the perpendicular vector.
-		/// </summary>
-		/// <returns></returns>
-		public XY Perpendicular()
-		{
-			return new XY(-this.Y, this.X);
-		}
-
-		/// <summary>
-		/// Obtains the polar point of another point. 
-		/// </summary>
-		/// <param name="distance">Distance from point u.</param>
-		/// <param name="angle">Angle in radians.</param>
-		/// <returns>The polar point of the specified point.</returns>
-		public XY Polar(double distance, double angle)
-		{
-			XY dir = new(Math.Cos(angle), Math.Sin(angle));
-			return this + dir * distance;
 		}
 
 		/// <inheritdoc/>
@@ -150,10 +102,40 @@ namespace CSMath
 			return this.X == other.X && this.Y == other.Y;
 		}
 
+		/// <summary>
+		/// Get the angle
+		/// </summary>
+		/// <returns>Angle in radians</returns>
+		public double GetAngle()
+		{
+			return Math.Atan2(Y, X);
+		}
+
 		/// <inheritdoc/>
 		public override int GetHashCode()
 		{
 			return this.X.GetHashCode() ^ this.Y.GetHashCode();
+		}
+
+		/// <summary>
+		/// Get the perpendicular vector.
+		/// </summary>
+		/// <returns></returns>
+		public XY Perpendicular()
+		{
+			return new XY(-this.Y, this.X);
+		}
+
+		/// <summary>
+		/// Obtains the polar point of another point.
+		/// </summary>
+		/// <param name="distance">Distance from point u.</param>
+		/// <param name="angle">Angle in radians.</param>
+		/// <returns>The polar point of the specified point.</returns>
+		public XY Polar(double distance, double angle)
+		{
+			XY dir = new(Math.Cos(angle), Math.Sin(angle));
+			return this + dir * distance;
 		}
 
 		/// <inheritdoc/>
@@ -171,6 +153,41 @@ namespace CSMath
 		public string ToString(IFormatProvider? cultureInfo)
 		{
 			return $"{X.ToString(cultureInfo)},{Y.ToString(cultureInfo)}";
+		}
+
+		/// <inheritdoc/>
+		public double this[int index]
+		{
+			get
+			{
+				switch (index)
+				{
+					case 0:
+						return X;
+
+					case 1:
+						return Y;
+
+					default:
+						throw new IndexOutOfRangeException($"The index must be between 0 and {this.Dimension}.");
+				}
+			}
+			set
+			{
+				switch (index)
+				{
+					case 0:
+						X = value;
+						break;
+
+					case 1:
+						Y = value;
+						break;
+
+					default:
+						throw new IndexOutOfRangeException($"The index must be between 0 and {this.Dimension}.");
+				}
+			}
 		}
 	}
 }
