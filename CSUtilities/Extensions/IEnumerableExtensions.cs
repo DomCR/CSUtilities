6283546d94be1aee98800f8cce10312f2d16bc22
DomCR <domenech93@gmail.com>
2026-02-28 10:42:32 +0100

    fix

--- CSUtilities/Extensions/IEnumerableExtensions.cs
+++ CSUtilities/Extensions/IEnumerableExtensions.cs
@@ -50,45 +50,44 @@ internal static class IEnumerableExtensions
 		return true;
 	}
 
-		/// <summary>
-		/// Returns a sequence containing all elements of the source collection except for consecutive elements equal to the
-		/// specified value at the end.
-		/// </summary>
-		/// <remarks>Equality is determined using the <see cref="object.Equals(object)"/> method. The order of
-		/// elements in the returned sequence matches the source collection. The operation is performed eagerly; the entire
-		/// collection is enumerated and copied before processing.</remarks>
-		/// <typeparam name="T">The type of elements in the source collection.</typeparam>
-		/// <param name="enumerable">The source collection from which to remove trailing elements equal to <paramref name="element"/>. Cannot be null.</param>
-		/// <param name="element">The value to compare against elements at the end of the collection. All consecutive elements equal to this value
-		/// at the end will be removed.</param>
-		/// <returns>An <see cref="IEnumerable{T}"/> containing the elements of the source collection with trailing elements equal to
-		/// <paramref name="element"/> removed. If no trailing elements match, the original sequence is returned.</returns>
-		public static IEnumerable<T> RemoveLastEquals<T>(this IEnumerable<T> enumerable, T element)
+	/// <summary>
+	/// Returns a sequence containing all elements of the source collection except for consecutive elements equal to the
+	/// specified value at the end.
+	/// </summary>
+	/// <remarks>Equality is determined using the <see cref="object.Equals(object)"/> method. The order of
+	/// elements in the returned sequence matches the source collection. The operation is performed eagerly; the entire
+	/// collection is enumerated and copied before processing.</remarks>
+	/// <typeparam name="T">The type of elements in the source collection.</typeparam>
+	/// <param name="enumerable">The source collection from which to remove trailing elements equal to <paramref name="element"/>. Cannot be null.</param>
+	/// <param name="element">The value to compare against elements at the end of the collection. All consecutive elements equal to this value
+	/// at the end will be removed.</param>
+	/// <returns>An <see cref="IEnumerable{T}"/> containing the elements of the source collection with trailing elements equal to
+	/// <paramref name="element"/> removed. If no trailing elements match, the original sequence is returned.</returns>
+	public static IEnumerable<T> RemoveLastEquals<T>(this IEnumerable<T> enumerable, T element)
+	{
+		List<T> lst = new List<T>(enumerable);
+		while (lst.Last().Equals(element))
 		{
-			List<T> lst = new List<T>(enumerable);
-			while (lst.Last().Equals(element))
-			{
-				lst.RemoveAt(lst.Count - 1);
-			}
-
-			return lst;
+			lst.RemoveAt(lst.Count - 1);
 		}
 
-		/// <summary>
-		/// Performs the specified action on each element of the source sequence.
-		/// </summary>
-		/// <remarks>This method enumerates the entire sequence and invokes the provided action for each element in
-		/// order. If the sequence is empty, the action is not invoked. The method does not modify the source
-		/// sequence.</remarks>
-		/// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
-		/// <param name="enumerable">The sequence of elements to iterate over. Cannot be null.</param>
-		/// <param name="action">The action to perform on each element of the sequence. Cannot be null.</param>
-		public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+		return lst;
+	}
+
+	/// <summary>
+	/// Performs the specified action on each element of the source sequence.
+	/// </summary>
+	/// <remarks>This method enumerates the entire sequence and invokes the provided action for each element in
+	/// order. If the sequence is empty, the action is not invoked. The method does not modify the source
+	/// sequence.</remarks>
+	/// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+	/// <param name="enumerable">The sequence of elements to iterate over. Cannot be null.</param>
+	/// <param name="action">The action to perform on each element of the sequence. Cannot be null.</param>
+	public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+	{
+		foreach (T item in enumerable)
 		{
-			foreach (T item in enumerable)
-			{
-				action(item);
-			}
+			action(item);
 		}
 	}
 }
