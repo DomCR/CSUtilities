4629039128f0b046fb2a7e9d13b76f53f8864282
DomCR <domenech93@gmail.com>
2026-05-29 11:27:34 +0200

    Add TryAdd extension method to IDictionaryExtension
    Added a new `TryAdd<TKey, TValue>` method to `IDictionaryExtension`.
    This method attempts to add a key-value pair to a dictionary only
    if the key does not already exist. Includes XML documentation
    detailing its purpose, usage, and behavior.

--- CSUtilities/Extensions/IDictionaryExtension.cs
+++ CSUtilities/Extensions/IDictionaryExtension.cs
@@ -21,5 +21,27 @@ public static class IDictionaryExtension
 
 		return false;
 	}
+
+	/// <summary>
+	/// Attempts to add the specified key and value to the dictionary if the key does not already exist.
+	/// </summary>
+	/// <remarks>This method does not overwrite an existing value for the specified key. If the key already exists
+	/// in the dictionary, the method returns false and does not modify the dictionary.</remarks>
+	/// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+	/// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+	/// <param name="dictionary">The dictionary to which the key and value should be added. Cannot be null.</param>
+	/// <param name="key">The key to add to the dictionary.</param>
+	/// <param name="value">The value to associate with the specified key.</param>
+	/// <returns>true if the key and value were added to the dictionary; otherwise, false.</returns>
+	public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+	{
+		if (dictionary.ContainsKey(key))
+		{
+			return false;
+		}
+
+		dictionary.Add(key, value);
+		return true;
+	}
 #endif
 }
