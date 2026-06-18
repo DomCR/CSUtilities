fdf1403ede6e3376a0baa1deac166cbe4d21262e
DomCR <domenech93@gmail.com>
2026-06-18 11:25:00 +0200

    Add IsEven and IsOdd extension methods to MathHelper
    Introduced IsEven and IsOdd extension methods for integers in the MathHelper class, using bitwise operations. Added XML documentation for both methods.

--- CSMath/MathHelper.cs
+++ CSMath/MathHelper.cs
@@ -253,6 +253,26 @@ public static class MathHelper
 	}
 
 	/// <summary>
+	/// Determines whether the specified integer is even.
+	/// </summary>
+	/// <param name="value">The integer to evaluate.</param>
+	/// <returns>True if the integer is even; otherwise, false.</returns>
+	public static bool IsEven(this int value)
+	{
+		return (value & 1) == 0;
+	}
+
+	/// <summary>
+	/// Determines whether the specified integer is odd.
+	/// </summary>
+	/// <param name="value">The integer to evaluate.</param>
+	/// <returns>True if the integer is odd; otherwise, false.</returns>
+	public static bool IsOdd(this int value)
+	{
+		return (value & 1) == 1;
+	}
+
+	/// <summary>
 	/// Checks if a number is close to zero.
 	/// </summary>
 	/// <param name="number">Double precision number.</param>
